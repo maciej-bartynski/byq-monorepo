ac636890cd32d9cba1666dff45b5e624984c7958
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2023-01-22 16:18:26 +0100

    proxy debugging part 3

--- frontend/services/AuthService/AuthService.js
+++ frontend/services/AuthService/AuthService.js
@@ -16,7 +16,7 @@ const AuthService = {
                     jwksRequestsPerMinute: 5,
                     jwksUri: `${authConfig.AUTH0_DOMAIN}/.well-known/jwks.json`
                 }),
-                audience: authConfig.audience,
+                audience: authConfig.AUTH0_DOMAIN,
                 issuer: `${authConfig.AUTH0_DOMAIN}/`,
                 algorithms: ['RS256'],
             })
